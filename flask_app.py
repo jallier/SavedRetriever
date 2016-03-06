6bad7cd85a5f9cc0f84f20c43a9c2bbabc470b27
Justin Hallier <justin.hallier+git@gmail.com>
2016-03-06 16:10:27 +1300

    Added settings page + added missing html files

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('index.html', posts={1, 2, 3, 4, 5})
+
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html')
 
 
 if __name__ == "__main__":
